70fdeb1ebde4fe6d228109d4fe6ab5f0fe9c5cb5
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-07-03 17:29:25 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -3,42 +3,15 @@ rainbow_colors = [
   "default",
   "light_cyan"
 ]
-#
-# def change_rainbow_colors
-#   # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
-#   rainbow_colors = [
-#     "yellow",
-#     "default",
-#     "light_cyan"
-#   ]
-#
-#   change_rainbow_colors = [
-#     rainbow_colors[0] = "red",
-#     rainbow_colors[1] = "light_red",
-#     rainbow_colors[2] = "light_yellow"
-#   ]
-# end
-#
-# def add_colors
-#   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
-#   rainbow_colors = [
-#     "yellow",
-#     "default",
-#     "light_cyan"
-#   ]
-#
-#   change_rainbow_colors = [
-#     rainbow_colors[0] = "red",
-#     rainbow_colors[1] = "light_red",
-#     rainbow_colors[2] = "light_yellow"
-#   ]
-#
-#   change_rainbow_colors << "green"
-#   change_rainbow_colors << "blue"
-# end
 
-def change_rainbow_colors(rainbow_colors)
+def change_rainbow_colors
   # Write a solution that corrects the three colors in rainbow_colors, then returns the corrected array
+  rainbow_colors = [
+    "yellow",
+    "default",
+    "light_cyan"
+  ]
+
   change_rainbow_colors = [
     rainbow_colors[0] = "red",
     rainbow_colors[1] = "light_red",
@@ -46,9 +19,21 @@ def change_rainbow_colors(rainbow_colors)
   ]
 end
 
-
-def add_colors(change_rainbow_colors)
+def add_colors
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
+  rainbow_colors = [
+    "yellow",
+    "default",
+    "light_cyan"
+  ]
+
+  change_rainbow_colors = [
+    rainbow_colors[0] = "red",
+    rainbow_colors[1] = "light_red",
+    rainbow_colors[2] = "light_yellow"
+  ]
+
   change_rainbow_colors << "green"
   change_rainbow_colors << "blue"
 end
+
