67a55658a3d8c7f592ba7424171062e374fa9a89
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-07-03 17:27:56 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -46,7 +46,6 @@ def change_rainbow_colors
   ]
 end
 
-puts change_rainbow_colors
 
 def add_colors
   # Write a solution that adds "green", then "blue" to the rainbow_colors array, then returns the array
